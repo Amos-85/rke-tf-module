f313325ef3bc50427cfaa0fd7ca33e97a756ca93
Amos Hadad <hadad22@netvision.net.il>
2020-08-02 03:27:49 +0300

    add hostname_override

--- rke.tf
+++ rke.tf
@@ -10,10 +10,10 @@ resource "rke_cluster" "cluster" {
     for_each = aws_instance.k8s-nodes
     content {
       address = nodes.value.public_dns
+      hostname_override = nodes.value.public_dns
       internal_address = nodes.value.private_ip
       user = var.user
-      role = [
-        lookup(nodes.value.tags, "Role")]
+      role = [lookup(nodes.value.tags, "Role")]
       ssh_key = file(var.rke_ec2_ssh_key)
     }
   }
